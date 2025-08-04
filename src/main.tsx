598f58728791b185603c8e9e0e38a23d6a012a9b
Victor Ozoterio <victorozoterio@gmail.com>
2025-08-04 17:34:00 -0300

    fix: corrige case-sensitive no App.tsx para deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.tsx';
+import App from './App.tsx';
 
 // biome-ignore lint/style/noNonNullAssertion: Ignoring because 'root' element is always present in the DOM.
 createRoot(document.getElementById('root')!).render(
